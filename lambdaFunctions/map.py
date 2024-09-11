bdcb2bfeb5165a755a4796fd75e0a61c924deef5
laeeq80 <laeeq80@gmail.com>
2024-09-11 11:31:17 +0500

    restructuring the practice topic wise

---
+++ lambdaFunctions/map.py
@@ -0,0 +1,5 @@
+def square(x):
+    return x ** 2
+List = [1,2,3]
+squares = list(map(square,List))
+print(squares)
---
+++ lambdaFunctions/mapWithLamda.py
@@ -0,0 +1,5 @@
+List=[1,2,3]
+
+# Using map with a lambda function to square each number
+squares = list(map((lambda x: x ** 2), List))
+print(squares)
